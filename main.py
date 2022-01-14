175433ebafc5902fd4eb69ad3f5f221408e211c0
nabil <merchant.nabil@gmail.com>
2022-01-13 22:02:16 -0600

    Adding songs to a spotify playlist.

--- main.py
+++ main.py
@@ -31,4 +31,19 @@ sp = spotipy.Spotify(
     )
 )
 user_id = sp.current_user()["id"]
-print(user_id)
+
+song_uris = []
+year = date.split("-")[0]
+
+for song in songs:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipping.")
+
+playlist = sp.user_playlist_create(user=user_id, name=f"Billboard 100 list on {date}", public=False)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
