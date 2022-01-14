13e93b31ded7f4a1e4763a4edb4e1bbb7666d48b
nabil <merchant.nabil@gmail.com>
2022-01-13 21:53:40 -0600

    Authenticating with spotify

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Spotify playlist
-For this project, I am using BeautifulSoup to scrape Billboards top 100 songs from a particular date that the user inputs in the format YYYY-MM-DD. Then I will extract the song titles and use the spotify API to add those songs to a playlist. 
+For this project, I am using BeautifulSoup to scrape Billboards top 100 songs from a particular date that the user inputs in the format YYYY-MM-DD. Then I will extract the song titles and use the spotify API to add those songs to a playlist.
+
+Spotify uses OAuth to allow 3rd party apps to access an account. I am using Spotipy, a python module for Spotify to make this easier. 
+https://spotipy.readthedocs.io
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+SPOTIPY_CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
+SPOTIPY_CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
 
 date = input("What year do you want to travel to? Type the date in the formal YYYY-MM-DD: ")
 URL = "https://www.billboard.com/charts/hot-100/" + date + "/"
@@ -12,3 +20,15 @@ soup = BeautifulSoup(website_html, "html.parser")
 all_songs = soup.find_all(name="div", class_="o-chart-results-list-row-container")
 songs = [song.find(name="h3", id="title-of-a-story").getText().strip() for song in all_songs]
 print(songs)
+
+# Authenticate with Spotify
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
